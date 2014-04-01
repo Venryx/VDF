ca107f66615484cc8fcbbca82d6232280a569ff2
Venryx <venryx@gmail.com>
2014-04-01 12:56:34 -0700

    Oh yeah; saver is significantly clearer/easier to understand, now.

--- VDF/VDF/VDF/VDFSaver.cs
+++ VDF/VDF/VDF/VDFSaver.cs
@@ -6,22 +6,29 @@ using System.Text;
 class VDFSaveNode
 {
 	public string metadata;
-	public List<object> children = new List<object>();
+	public List<object> items = new List<object>(); 
+	public Dictionary<string, VDFSaveNode> properties = new Dictionary<string, VDFSaveNode>();
 	public bool popOutToOwnLine;
 	public bool isFirstItemOfNonFirstPopOutGroup;
 	public bool isNonFirstItemOfArray;
 	public string GetInLineItemText()
 	{
 		var builder = new StringBuilder();
-		foreach (object child in children)
+		foreach (object item in items)
 		{
-			if (child is VDFSaveNode)
+			if (item is VDFSaveNode)
 			{
-				if (!((VDFSaveNode)child).popOutToOwnLine)
-					builder.Append(((VDFSaveNode)child).GetInLineItemText());
+				if (!((VDFSaveNode)item).popOutToOwnLine)
+					builder.Append(((VDFSaveNode)item).GetInLineItemText());
 			}
 			else
-				builder.Append(child);
+				builder.Append(item);
+		}
+		foreach (string propName in properties.Keys)
+		{
+			VDFSaveNode propValueNode = properties[propName];
+			if (!propValueNode.popOutToOwnLine)
+				builder.Append(propName + "{" + propValueNode.GetInLineItemText() + "}");
 		}
 		return (isFirstItemOfNonFirstPopOutGroup ? "#" : "") + (isNonFirstItemOfArray && !popOutToOwnLine ? "|" : "") + (metadata != null ? "<" + metadata + ">" : "") + builder; // markers + metadata + data
 	}
@@ -30,15 +37,24 @@ class VDFSaveNode
 		var lines = new List<string>();
 		if (popOutToOwnLine)
 			lines.Add(GetInLineItemText());
-		foreach (object child in children)
-			if (child is VDFSaveNode)
+		foreach (object item in items)
+		{
+			if (item is VDFSaveNode)
 			{
-				var childAsNode = (VDFSaveNode)child;
-				string poppedOutText = childAsNode.GetPoppedOutItemText();
+				string poppedOutText = ((VDFSaveNode)item).GetPoppedOutItemText();
 				if (poppedOutText.Length > 0)
 					foreach (string line in poppedOutText.Split(new[] { '\n' }))
 						lines.Add(line);
 			}
+		}
+		foreach (string propName in properties.Keys)
+		{
+			VDFSaveNode propValueNode = properties[propName];
+			string poppedOutText = propValueNode.GetPoppedOutItemText();
+			if (poppedOutText.Length > 0)
+				foreach (string line in poppedOutText.Split(new[] { '\n' }))
+					lines.Add(line);
+		}
 		var builder = new StringBuilder();
 		for (int i = 0; i < lines.Count; i++)
 			builder.Append(i == 0 ? "" : "\n").Append(popOutToOwnLine ? "\t" : "").Append(lines[i]); // line-breaks + indents + data
@@ -56,11 +72,11 @@ static class VDFSaver
 
 		Type type = obj.GetType();
 		if (VDF.typeExporters_inline.ContainsKey(type))
-			objNode.children.Add(VDF.typeExporters_inline[type](obj));
+			objNode.items.Add(VDF.typeExporters_inline[type](obj));
 		else if (type == typeof(float) || type == typeof(double))
-			objNode.children.Add(obj.ToString().StartsWith("0.") ? obj.ToString().Substring(1) : obj.ToString());
+			objNode.items.Add(obj.ToString().StartsWith("0.") ? obj.ToString().Substring(1) : obj.ToString());
 		else if (type.IsPrimitive || type == typeof(string))
-			objNode.children.Add(obj.ToString());
+			objNode.items.Add(obj.ToString());
 		else if (obj is IList)
 		{
 			var objAsList = (IList)obj;
@@ -73,7 +89,7 @@ static class VDFSaver
 					itemValueNode.isNonFirstItemOfArray = true;
 				if (typeDerivedFromDeclaredType)
 					itemValueNode.metadata = item.GetType().Name;
-				objNode.children.Add(itemValueNode);
+				objNode.items.Add(itemValueNode);
 			}
 		}
 		else
@@ -95,26 +111,24 @@ static class VDFSaver
 				bool typeDerivedFromDeclaredType = propValue.GetType() != propInfo.propType; // if value is of a type *derived* from the property's base value-type (i.e. we need to specify actual value-type)
 				if (propInfo.popOutItemsToOwnLines)
 				{
-					objNode.children.Add(name + "{#}");
 					VDFSaveNode propValueNode = ToVDFSaveNode(propValue);
+					propValueNode.items.Add("#");
 					if (popOutGroupsAdded > 0)
-						((VDFSaveNode)propValueNode.children[0]).isFirstItemOfNonFirstPopOutGroup = true;
+						((VDFSaveNode)propValueNode.items[0]).isFirstItemOfNonFirstPopOutGroup = true;
 					if (typeDerivedFromDeclaredType)
 						propValueNode.metadata = propValue.GetType().Name;
-					foreach (object child in propValueNode.children)
-						if (child is VDFSaveNode)
-							((VDFSaveNode)child).popOutToOwnLine = true;
-					objNode.children.Add(propValueNode);
+					foreach (object propValueNodeItem in propValueNode.items)
+						if (propValueNodeItem is VDFSaveNode)
+							((VDFSaveNode)propValueNodeItem).popOutToOwnLine = true;
+					objNode.properties.Add(name, propValueNode);
 					popOutGroupsAdded++;
 				}
 				else
 				{
-					objNode.children.Add(name + "{");
 					VDFSaveNode propValueNode = ToVDFSaveNode(propValue);
 					if (typeDerivedFromDeclaredType)
 						propValueNode.metadata = propValue.GetType().Name;
-					objNode.children.Add(propValueNode);
-					objNode.children.Add("}");
+					objNode.properties.Add(name, propValueNode);
 				}
 			}
 		}
