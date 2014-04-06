ffd333703b5305c3f239ba886a4c3f633b4d4173
Venryx <venryx@gmail.com>
2014-04-05 17:54:39 -0700

    Testing system ready to use!

--- VDF/VDFTests/Properties/AssemblyInfo.cs
+++ VDF/VDFTests/Properties/AssemblyInfo.cs
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("014b8584-4d10-4873-be30-ffe563fc27dc")]
+[assembly: Guid("e11db86b-b00f-47ce-aa0c-140dc11bbe4d")]
 
 // Version information for an assembly consists of the following four values:
 //
